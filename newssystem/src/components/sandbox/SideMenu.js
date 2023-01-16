8751ebcfb171243ac460e65afc87f7036a16c091
gilbert777 <xjt674471772@mail.com>
2023-01-16 15:49:18 +1100

    后端SideMenu

--- newssystem/src/components/sandbox/SideMenu.js
+++ newssystem/src/components/sandbox/SideMenu.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
 
 
@@ -16,79 +16,59 @@ import {
 
 } from '@ant-design/icons';
 import './index.css'
-import {withRouter} from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
 import { Layout, Menu, theme } from 'antd';
+import axios from 'axios'
 const { Header, Sider, Content } = Layout;
-function getItem(label, key, icon, children, type) {
-  return {
-    label, key, icon, children, type
-  };
-}
-
-//模拟数组结构
-const items = [
-  getItem('Option 1', '1', <PieChartOutlined />),
-  getItem('Option 2', '2', <DesktopOutlined />),
-  getItem('Option 3', '3', <ContainerOutlined />),
-  getItem('Navigation One', 'sub1', <MailOutlined />, [
-    getItem('Option 5', '5'),
-    getItem('Option 6', '6'),
-    getItem('Option 7', '7'),
-    getItem('Option 8', '8'),
-  ]),
-  getItem('Navigation Two', 'sub2', <AppstoreOutlined />, [
-    getItem('Option 9', '9'),
-    getItem('Option 10', '10'),
-    getItem('Submenu', 'sub3', null, [getItem('Option 11', '11'), getItem('Option 12', '12')]),
-  ]),
-];
-
-//模拟数组结构
-const  menuList = [
-  {
-    key:"/home",
-    label:"首页",
-    icon:<UserOutlined />
-  },
-  {
-    key:"/user-manage",
-    label:"用户管理",
-    icon:<UserOutlined />,
-    children:[
-      {
-        key:"/user-manage/list",
-        label:"用户列表",
-        icon:<UserOutlined />
-      }
-    ]
-  },
-  {
-    key:"/right-manage",
-    label:"权限管理",
-    icon:<UserOutlined />,
-    children:[
-      {
-        key:"/right-manage/role/list",
-        label:"角色列表",
-        icon:<UserOutlined />
-      },
-      {
-        key:"/right-manage/right/list",
-        label:"权限列表",
-        icon:<UserOutlined />
-      }
-    ]
-  }
-]
 
 function SideMenu(props) {
+  const [menu, setMenu] = useState([])
 
-  const renderMenu = ()=>{
-    console.log(items);
-    return menuList
-      
+  //第一次渲染画面
+  useEffect(() => {
+    axios.get("http://localhost:3000/rights?_embed=children").then(res => {
+      setMenu(dfs1(res.data))
+    })
+  }, [])
+
+  //生成渲染数组的对象
+  const obj = (key, icon, label, children) => {
+    return {
+      key,
+      icon,
+      label,
+      children,
+    }
   }
 
+  //图标映射对象
+  var iconList = {
+    "/home":<UserOutlined />,
+    "/user-manage": <UserOutlined />,
+    "/user-manage/list": <UserOutlined />,
+    "/right-manage": <UserOutlined />,
+    "/right-manage/role/list": <UserOutlined />,
+    "/right-manage/right/list": <UserOutlined />
+    //.......
+  }
+
+  //渲染符合条件的数组对象
+  const dfs1 = (list) => {
+    const arr = []
+    list.map((item) => {
+      if (item.children && item.children.length !== 0) {
+        arr.push(obj(item.key, iconList[item.key], item.label, dfs1(item.children)))
+      } else {
+        if (item.pagepermisson) {
+          arr.push(obj(item.key, iconList[item.key], item.label))
+        }
+      }
+    })
+    return arr
+  }
+
+
+  //点击跳转对应组件
   const onClick = (e) => {
     props.history.push(e.key)
   };
@@ -102,25 +82,7 @@ function SideMenu(props) {
         mode="inline"
         theme="dark"
         onClick={onClick}
-        items={renderMenu()
-          /* [
-          {
-            key: '1',
-            icon: <UserOutlined />,
-            label: '首页',
-          },
-          {
-            key: '2',
-            icon: <VideoCameraOutlined />,
-            label: '用户管理',
-          },
-          {
-            key: '3',
-            icon: <UploadOutlined />,
-            label: '',
-          },
-        ] */
-        }
+        items={menu}
       />
     </Sider>
   )
