a1691057078610f2efcacd1d407c4c4158ac8cd3
gilbert777 <xjt674471772@mail.com>
2023-01-28 16:26:00 +1100

    路由权限-本地映射表

--- newssystem/src/views/sandbox/NewsSandBox.js
+++ newssystem/src/views/sandbox/NewsSandBox.js
@@ -1,51 +1,33 @@
 import React from 'react'
-import { Switch, Route, Redirect } from 'react-router-dom'
 import SideMenu from '../../components/sandbox/SideMenu'
 import TopHeader from '../../components/sandbox/TopHeader'
-import Home from './home/Home'
-import Nopermission from './nopermission/Nopermission'
-import RightList from './right-manage/RightList'
-import RoleList from './right-manage/RoleList'
-import UserList from './user-manage/UserList'
 
 //css
 import './NewsSandBox.css'
+
 //antd
 import { Layout } from 'antd'
-const { Header, Sider, Content } = Layout;
+import NewsRouter from '../../components/sandbox/NewsRouter'
+const {Content} = Layout
+
 export default function NewsSandBox() {
-  return (
-    <Layout>
-      <SideMenu></SideMenu>
-
-      <Layout className="site-layout">
-        <TopHeader></TopHeader>
-        <Content
-        style={{
-          margin: '24px 16px',
-          padding: 24,
-          minHeight: 280,
-          overflow:'auto'
-        }}
-      >
-        <Switch>
-          <Route path="/home" component={Home} />
-          <Route path="/user-manage/list" component={UserList} />
-          <Route path="/right-manage/role/list" component={RoleList} />
-          <Route path="/right-manage/right/list" component={RightList} />
-          {/* 重定向 */}
-          <Redirect from="/" to="/home" exact />
-          <Route path="*" component={Nopermission} />
-        </Switch>
-        
-      </Content>
-      </Layout>
-
-      
-
-
-      
-    </Layout>
-
-  )
+    return (
+        <Layout>
+            <SideMenu></SideMenu>
+            <Layout className="site-layout">
+                <TopHeader></TopHeader>
+                <Content
+                    className="site-layout-background"
+                    style={{
+                        margin: '24px 16px',
+                        padding: 24,
+                        minHeight: 280,
+                        overflow:"auto"
+                    }}
+                >
+                   <NewsRouter></NewsRouter>
+                </Content>
+            </Layout>
+        </Layout>
+    )
 }
