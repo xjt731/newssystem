4b8c3a8fa14e9a82ae4bf60b1edb7859872794f4
gilbert777 <xjt674471772@mail.com>
2023-02-02 12:32:51 +1100

    首页数据展示-排版

--- newssystem/src/views/sandbox/home/Home.js
+++ newssystem/src/views/sandbox/home/Home.js
@@ -1,9 +1,56 @@
 import React from 'react'
+import { Card, Col, Row, List,Avatar } from 'antd';
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+const { Meta } = Card;
 
+// import axios from 'axios'
 export default function Home() {
+
     return (
-        <div>
-            home
+        <div className="site-card-wrapper">
+            <Row gutter={16}>
+                <Col span={8}>
+                    <Card title="用户最常浏览" bordered={true}>
+                        <List
+                            size="small"
+                            // bordered
+                            dataSource={["1111", "222", "33333"]}
+                            renderItem={item => <List.Item>{item}</List.Item>}
+                        />
+                    </Card>
+                </Col>
+                <Col span={8}>
+                    <Card title="用户点赞最多" bordered={true}>
+                        <List
+                            size="small"
+                            // bordered
+                            dataSource={["1111", "222", "33333"]}
+                            renderItem={item => <List.Item>{item}</List.Item>}
+                        />
+                    </Card>
+                </Col>
+                <Col span={8}>
+                    <Card
+                        cover={
+                            <img
+                                alt="example"
+                                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                            />
+                        }
+                        actions={[
+                            <SettingOutlined key="setting" />,
+                            <EditOutlined key="edit" />,
+                            <EllipsisOutlined key="ellipsis" />,
+                        ]}
+                    >
+                        <Meta
+                            avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                            title="Card title"
+                            description="This is the description"
+                        />
+                    </Card>
+                </Col>
+            </Row>
         </div>
     )
 }
