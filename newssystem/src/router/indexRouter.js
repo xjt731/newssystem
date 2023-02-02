06d8ce952036dae365fbc1f05110b6a4ddddb729
gilbert777 <xjt674471772@mail.com>
2023-02-02 16:33:27 +1100

    游客系统-路由

--- newssystem/src/router/indexRouter.js
+++ newssystem/src/router/indexRouter.js
@@ -2,6 +2,8 @@ import { Redirect, Route,Switch } from 'react-router-dom'
 import React from 'react'
 import {HashRouter} from 'react-router-dom'
 import Login from '../views/login/Login'
+import Detail from '../views/news/Detail'
+import News from '../views/news/News'
 import NewsSandBox from '../views/sandbox/NewsSandBox'
 export default function indexRouter() {
   return (
@@ -9,6 +11,8 @@ export default function indexRouter() {
           {/* 模糊匹配 */}
           <Switch>
             <Route path='/login' component={Login}/>
+            <Route path='/news' component={News}/>
+            <Route path='/details/:id' component={Detail}/>
             {/* <Route path='/' component={NewsSandBox}/> */}
             <Route path='/' render={()=>{
               return localStorage.getItem('ss')?
